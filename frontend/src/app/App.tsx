5f78216aa52f4b9c107f537838ecda3b6227c325
immortal-p <immortalkil4oy@gmail.com>
2026-01-02 22:49:02 +0300

    refactor: removing global header

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,13 +1,10 @@
 import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom'
-import Header from '@/components/layout/Header/Header'
 import HomePage from '@/pages/Home/HomePage'
 
 function RouterLayout() {
     return (
         <div className="min-h-screen flex flex-col bg-(--background) relative overflow-hidden items-center">
-            <Header />
             <Outlet />
-            {/* <Foother /> */}
         </div>
     )
 }
@@ -18,7 +15,6 @@ const App = () => {
             <Routes>
                 <Route element={<RouterLayout />}>
                     <Route path='/' element={<HomePage />} />
-                    {/* <Route path='/skills' element={<Skills />} /> */}
                 </Route>
             </Routes>
         </BrowserRouter>
