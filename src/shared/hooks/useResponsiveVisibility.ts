e0f9d4d53b617403463a0f2015ec8b96fac83a7f
immortal-p <immortalkil4oy@gmail.com>
2026-01-09 21:57:18 +0300

    feat(animation): add responsive visibility and reduced motion hooks

---
+++ src/shared/hooks/useResponsiveVisibility.ts
@@ -0,0 +1,8 @@
+import { BREAKPOINTS, type BreakpointKey } from "../config/breakpoints";
+import { useMediaQuery } from "./useMediaQuery";
+
+export const useResponsiveVisibility = (
+    hideBelow?: BreakpointKey,
+): boolean => {
+    return hideBelow ? useMediaQuery(BREAKPOINTS[hideBelow]) : false
+}
