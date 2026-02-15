2dbc3a4d4d81fb88fdbe78613118ecaf9acc0849
immortal-p <immortalkil4oy@gmail.com>
2026-02-15 12:14:02 +0300

    fix(InconsInfo): installing a correct link and removing unnecessary code

--- src/components/ui/decor/IconsInfo.tsx
+++ src/components/ui/decor/IconsInfo.tsx
@@ -49,7 +49,7 @@ const IconInfo = () => {
                         />
                     </svg>
                 </Link>
-                <Link to={'https://web.telegram.org/k/#@Spru71k'}>
+                <Link to={'https://t.me/Spru71k'} target='_blank'>
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         width="31"
